edfa4cbd5d8a739fd61a14d91005dcdf524b7165
Joffre M. <joffre.molina@gmail.com>
2025-06-21 22:48:43 -0400

    Initial commit

--- tetris.py
+++ tetris.py
@@ -6,7 +6,7 @@ pygame.init()
 
 # set up the display
 screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption("My Super Game")
+pygame.display.set_caption("Tetris V 0.1")
 
 # Main Loop
 running = True
